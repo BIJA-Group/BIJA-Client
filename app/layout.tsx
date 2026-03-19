8859fbd3160eb906fdfa9093243ad5da44c5f381
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2026-03-19 22:31:18 +0200

    Installed the Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,16 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${dmSerifDisplay.variable} ${dmSans.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TBVH8SW5');`}
+        </Script>
+      </head>
       <body>
         <Navbar />
         <main>{children}</main>
@@ -92,6 +102,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             gtag('config', 'G-G-5T2S3SXRC0');
           `}
         </Script>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TBVH8SW5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
       </body>
     </html>
   );
